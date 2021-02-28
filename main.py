cfa7aa19af57bb075a58867cc9fe5290ab7d47e5
Arunagiri0512 <67681860+Arunagiri0512@users.noreply.github.com>
2021-02-28 20:23:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+a=input("enter your age:")
+while(true):
+    if a>=100:
+        print ("invalid data")
+    elif a<0:
+        print ("invalid data")
+    elif:
+        if a>=18:
+            print ("You can eligible to vote")
+        else:
+            print ("you cannot eligible to vote")
+pass
